7ca2b04605d1e63c57c845779a66fd6dfe82b718
toast-uz <tatsuzo.osawa@gmail.com>
2022-08-05 20:05:34 +0900

    Step3+ refactored, Env data uses vec.

--- impls/myrust/env.rs
+++ impls/myrust/env.rs
@@ -1,15 +1,14 @@
-use std::collections::HashMap;
 use crate::types::*;
 use crate::malerr;
 
 #[derive(Debug, Clone)]
 pub struct Env<'a> {
     outer: Option<&'a Env<'a>>,
-    data: HashMap<String, MalType>,
+    data: Vec<(String, MalType)>,
 }
 
 impl<'a> Env<'a> {
-    pub fn new(outer: Option<&'a Env>) -> Self { Self { outer: outer, data: HashMap::new(), } }
+    pub fn new(outer: Option<&'a Env>) -> Self { Self { outer, data: Vec::new(), } }
 
     pub fn init(&mut self, bind: &MalType, exprs: &MalType) {
         let bind = bind.list().unwrap();
@@ -23,7 +22,7 @@ impl<'a> Env<'a> {
 
     // takes a symbol key and a mal value and adds to the data structure
     pub fn set(&mut self, key: &str, value: &MalType) {
-        self.data.insert(key.to_string(), value.clone());
+        self.data.push((key.to_string(), value.clone()));
     }
 
     // takes a symbol key and if the current environment contains that key
@@ -32,7 +31,9 @@ impl<'a> Env<'a> {
     fn find(&self, key: &str) -> Option<&'a Env> {
         let mut env = self;
         loop {
-            if env.data.contains_key(key) { return Some(env); }
+            if env.data.iter().rev().find(|&x| x.0 == key).is_none() {
+                    return Some(env);
+            }
             if env.outer.is_none() { break; }
             env = env.outer.unwrap();
         }
@@ -43,7 +44,9 @@ impl<'a> Env<'a> {
     // then returns the matching value.
     // If no key is found up the outer chain, then throws/raises a "not found" error.
     pub fn get(&self, key: &str) -> Result<MalType> {
-        self.find(key).and_then(|env| env.data.get(key).cloned())
+        self.find(key).and_then(|env| env.data.iter().rev()
+            .find(|&x| x.0 == key)
+            .map(|x| x.1.clone()))
             .ok_or_else(|| malerr!("'{}' not found.", key))
     }
 }
