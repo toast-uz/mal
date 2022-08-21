ccd0eca0d69f68e935b8029ce6305616ca30ea53
toast-uz <tatsuzo.osawa@gmail.com>
2022-08-21 20:07:29 +0900

    Step4 completed.

--- impls/myrust/printer.rs
+++ impls/myrust/printer.rs
@@ -1,9 +1,17 @@
+use itertools::Itertools;
 use crate::types::*;
 
+// ListVecしか対応していないため、HashMapにバグがある
 pub fn pr_str(maltype: &MalType, print_readably: bool) -> String {
     if print_readably && maltype.string().is_some() {
 //        eprintln!("String:{}", maltype);
         format!("\"{}\"", maltype.to_string().escape_default().to_string())
+    } else if let MalType::ListVec(mlv) = maltype {
+        if mlv.0 {
+            format!("({})", mlv.1.iter().map(|x| pr_str(x, print_readably)).join(" "))
+        } else {
+            format!("[{}]", mlv.1.iter().map(|x| pr_str(x, print_readably)).join(" "))
+        }
     } else {
         format!("{}", maltype)
     }
